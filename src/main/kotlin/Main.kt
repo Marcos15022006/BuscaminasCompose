1e5cf78ec49f6d15eb6c6884e8b4c269dff29198
A24MarcosRG <a24marcosrg@iessanclemente.net>
2025-05-05 09:09:42 +0200

    busc comp

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.Alignment
@@ -14,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.ui.input.pointer.pointerInput
 import kotlinx.coroutines.delay
 import java.text.SimpleDateFormat
 import java.util.*
@@ -79,31 +81,46 @@ fun BuscaminasApp() {
             tablero.forEachIndexed { i, fila ->
                 Row {
                     fila.forEachIndexed { j, _ ->
-                        CeldaUI(tablero[i][j], tablero, i, j, onRightClick = {
-                            if (tablero[i][j].marcada) return@CeldaUI
-                            tablero = tablero.map { it.copyOf() }.toTypedArray()
-                            buscaminas.descubrirCelda(tablero, i, j)
-                            if (buscaminas.comprobarDerrota(tablero, i, j)) {
-                                estadoJuego = "¡Perdiste!"
-                            } else if (buscaminas.comprobarVictoria(tablero)) {
-                                estadoJuego = "¡Ganaste!"
+                        CeldaUI(
+                            celda = tablero[i][j],
+                            tablero = tablero,
+                            x = i,
+                            y = j,
+                            onLongClick = {
+                                tablero = tablero.map { it.copyOf() }.toTypedArray()
+                                if (!tablero[i][j].descubierta) {
+                                    tablero[i][j].marcar()
+                                }
+                            },
+                             onClick = {
+                                if (tablero[i][j].marcada) return@CeldaUI
+                                tablero = tablero.map { it.copyOf() }.toTypedArray()
+                                buscaminas.descubrirCelda(tablero, i, j)
+                                if (buscaminas.comprobarDerrota(tablero, i, j)) {
+                                    estadoJuego = "¡Perdiste!"
+                                } else if (buscaminas.comprobarVictoria(tablero)) {
+                                    estadoJuego = "¡Ganaste!"
+                                }
                             }
-                        }, onClick = {
-                            tablero = tablero.map { it.copyOf() }.toTypedArray()
-                            if (!tablero[i][j].descubierta) {
-                                tablero[i][j].marcar()
-                            }
-                        })
+                        )
                         Spacer(modifier = Modifier.width(2.dp))
                     }
                 }
                 Spacer(modifier = Modifier.height(4.dp))
             }
         }
+
     }
 }
 @Composable
-fun CeldaUI(celda: Celda, tablero: Array<Array<Celda>>, x: Int, y: Int, onClick: () -> Unit, onRightClick: () -> Unit) {
+fun CeldaUI(
+    celda: Celda,
+    tablero: Array<Array<Celda>>,
+    x: Int,
+    y: Int,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit
+) {
     val color = when {
         celda.descubierta && celda.tieneMina() -> Color.Red
         celda.descubierta -> Color.LightGray
@@ -115,11 +132,14 @@ fun CeldaUI(celda: Celda, tablero: Array<Array<Celda>>, x: Int, y: Int, onClick:
         modifier = Modifier
             .size(40.dp)
             .background(color, shape = RoundedCornerShape(4.dp))
-            .clickable(onClick = onClick)
-            .clickable(onClick = onRightClick, indication = null, interactionSource = remember { MutableInteractionSource() }),
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = { onClick() },
+                    onLongPress = { onLongClick() }
+                )
+            },
         contentAlignment = Alignment.Center
-    )
-    {
+    ) {
         if (celda.descubierta) {
             Text(if (celda.tieneMina()) "*" else celda.calcularminasAlrededor(tablero, x, y).toString())
         } else if (celda.marcada) {
